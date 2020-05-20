09e1c9086e28f08664a5236345ce97e8c029548a
evan <1137299673@qq.com>
2020-05-20 16:17:02 +0800

    :tada: Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lets Spring
+Some awesome examples to describe spring use cases.
+
+## Table of Content
+- [Java Unit Test and Mock Test](\lets-java-test\README.md)
+- [Spring Bean](\lets-spring-beans\README.md)
---
+++ lets-spring-web-mvc/src/main/java/io/github/wdpm/controller/RestController.java
@@ -0,0 +1,19 @@
+package io.github.wdpm.controller;
+
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author evan
+ * @date 2020/5/20
+ */
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/json")
+    public String json() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("hello", "world");
+        return jsonObject.toString();
+    }
+}
